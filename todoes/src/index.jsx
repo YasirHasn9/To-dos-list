940e3979d4b174ff59f5fd03a9ac4db1581c93ee
YasirHasn9 <hassanyasir223@gmail.com>
2019-09-22 14:29:25 -0700

    set up

--- todoes/src/index.jsx
+++ todoes/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/App";
+import App from "./component/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
